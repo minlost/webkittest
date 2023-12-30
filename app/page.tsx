95f4ecb7ce227a70f821856653d3a05ca5960bc3
Vaclav Vlcek <vlcek@1webit.com>
2023-12-30 13:14:09 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,10 @@ export default function Home() {
       <div className=" border-blue-500 ">
         <div className="bg-red-200 w-full">this is header</div>
       </div>
-      <button onClick={() => setOpen(!open)} className="bg-blue-200 h-64 w-20">
+      <button
+        onClick={() => setOpen(!open)}
+        className="bg-blue-200 h-64 w-full"
+      >
         open
       </button>
       <div className="bg-blue-200 h-64">next part</div>{" "}
@@ -18,7 +21,7 @@ export default function Home() {
       <div className="bg-blue-200 h-64">next part</div>
       <div className="bg-red-500 w-full ">this bottom part</div>
       {open && (
-        <div className="w-[30%] bg-red-500 absolute right-0 top-0 h-full">
+        <div className="w-[30%] bg-red-500 fixed right-0 top-0 h-full">
           sidebar
         </div>
       )}
