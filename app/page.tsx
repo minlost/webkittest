1b14f39be1f9cfbacf71995ef0712b1ef6d07764
Vaclav Vlcek <vlcek@1webit.com>
2023-12-30 13:12:47 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="h-full">
       <div className=" border-blue-500 ">
         <div className="bg-red-200 w-full">this is header</div>
       </div>
+      <button onClick={() => setOpen(!open)} className="bg-blue-200 h-64 w-20">
+        open
+      </button>
       <div className="bg-blue-200 h-64">next part</div>{" "}
       <div className="bg-blue-200 h-64">next part</div>
       <div className="bg-red-200 h-64">next part</div>
       <div className="bg-blue-200 h-64">next part</div>
       <div className="bg-red-500 w-full ">this bottom part</div>
-      <div className="w-[30%] bg-red-200 absolute right-0 top-0 h-full">
-        sidebar
-      </div>
+      {open && (
+        <div className="w-[30%] bg-red-500 absolute right-0 top-0 h-full">
+          sidebar
+        </div>
+      )}
     </div>
   );
 }
