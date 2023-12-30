70ebb164e61f067bcc252e08c1c93259d09c5617
Vaclav Vlcek <vlcek@1webit.com>
2023-12-30 13:09:51 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="bg-red-200 h-64">next part</div>
       <div className="bg-blue-200 h-64">next part</div>
       <div className="bg-red-500 w-full ">this bottom part</div>
-      <div className="w-[30%] bg-yellow-200 absolute right-0 top-0 h-full">
+      <div className="w-[30%] bg-red-200 absolute right-0 top-0 h-full">
         sidebar
       </div>
     </div>
