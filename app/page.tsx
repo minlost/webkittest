c767436d8b2843223437e9fd0bba4f3637d73a01
Vaclav Vlcek <vlcek@1webit.com>
2023-12-30 12:53:54 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 export default function Home() {
   return (
-    <div className="">
-      <div className="min-h-screen flex flex-col">
-        <main className="flex-grow">
-          <div className="bg-red-200 w-full">this is header</div>
-        </main>
-        <div className="bg-red-500 w-full mt-auto">
-          this bottom part of 100vh
-        </div>
+    <div className="h-full">
+      <div className=" border-blue-500 ">
+        <div className="bg-red-200 w-full">this is header</div>
       </div>
+      <div className="bg-blue-200 h-64">next part</div>{" "}
       <div className="bg-blue-200 h-64">next part</div>
+      <div className="bg-red-200 h-64">next part</div>
+      <div className="bg-blue-200 h-64">next part</div>
+      <div className="bg-red-500 w-full ">this bottom part</div>
     </div>
   );
 }
